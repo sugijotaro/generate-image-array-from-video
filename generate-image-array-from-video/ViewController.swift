2ac63df78e5daf442fb69c536114d4d06a86ccfa
Jotaro Sugiyama <sugi.jotaro@gmail.com>
2023-02-18 15:49:23 +0900

    Initial Commit

---
+++ generate-image-array-from-video/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  generate-image-array-from-video
+//
+//  Created by JotaroSugiyama on 2023/02/18.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
